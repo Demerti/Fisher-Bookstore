1216a6f6a514561b802404d10f5701b796a333bc
Kane Vastano <kmvastano@gmail.com>
2019-03-15 15:45:09 -0400

    The Book Component

---
+++ web/src/app/books/books/books.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Book } from '../books/book';
+
+@Component({
+  selector: 'app-books',
+  templateUrl: './books.component.html',
+  styleUrls: ['./books.component.css']
+})
+export class BooksComponent implements OnInit {
+
+  book: Book = {
+    id:1,
+    title: 'Ready Player One',
+    author: 'Ernest Cline'
+  }
+
+  ngOnInit() {
+  }
+
+}
